6df7f86fec8a4bb3477cab55433521c051c69ef3
chris hill <chris.hill@bellaliant.net>
2017-06-10 19:10:41 -0300

    Minor tweaks to Csv parser

--- ClimateMaps/app/src/main/java/edu/school/nick/climatemaps/CSVParser.java
+++ ClimateMaps/app/src/main/java/edu/school/nick/climatemaps/CSVParser.java
@@ -1,20 +1,10 @@
 package edu.school.nick.climatemaps;
 
 
-import android.os.Debug;
-import android.support.annotation.NonNull;
-import android.util.Log;
-
 import java.io.BufferedReader;
-import java.io.Externalizable;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by dooml on 6/10/2017.
@@ -25,12 +15,13 @@ public class CSVParser {
     String line;
 
 
-    public ArrayList<ClimateData> ParseData(File csvFile) throws Exception {
+    public ArrayList<ClimateData> ParseData() throws Exception {
+        String csvString = "NS_Climate_Change_Data.csv";
         ArrayList<ClimateData> returnData = new ArrayList<ClimateData>();
         boolean firstLine = true;
-        if(csvFile.canRead() && csvFile.exists()) {
+        if(csvString != null) {
             try {
-                bufferedReader = new BufferedReader(new FileReader(csvFile));
+                bufferedReader = new BufferedReader(new FileReader(csvString));
                 while ( (line = bufferedReader.readLine()) != null) {
                     if(firstLine) {
                         firstLine = false;
@@ -38,14 +29,11 @@ public class CSVParser {
                     }
 
                     String[] data = line.split(",");
-                    String[] param = null;
 
                     double unit = Double.parseDouble(data[4]);
-                    String paramString = param[0].trim();
-                    String subType = param[1].trim();
                     int rangeInt = Integer.parseInt(data[3].substring(0, (data[3].length() - 1)));
 
-                    ClimateData row = new ClimateData(data[0], data[1], data[2], rangeInt, unit);
+                    ClimateData row = new ClimateData(data[0].trim(), data[1].trim(), data[2].trim(), rangeInt, unit);
 
                     returnData.add(row);
                 }
